583370f5556d0c84e969083c68909838cc8fffc4
hpbock <hpbock@be94bdbd-1fe7-0310-aeeb-c95d3c307607>
2003-05-09 11:27:59 +0000

    Message to realtime txqueue manager is now sent in rtnetif_wake_queue().

--- branches/branch-2003-04-15-rtmac-framework/rtnet/include/rtnet_port.h
+++ branches/branch-2003-04-15-rtmac-framework/rtnet/include/rtnet_port.h
@@ -1,6 +1,8 @@
 #ifndef _RTNET_PORT_H_
 #define _RTNET_PORT_H_
 
+#include "../rtmac_mgr.h"
+
 static inline void rtnetif_start_queue(struct rtnet_device *rtdev)
 {
 	clear_bit(__LINK_STATE_XOFF, &rtdev->dev->state);
@@ -8,8 +10,13 @@ static inline void rtnetif_start_queue(struct rtnet_device *rtdev)
 
 static inline void rtnetif_wake_queue(struct rtnet_device *rtdev)
 {
-    if (test_and_clear_bit(__LINK_STATE_XOFF, &rtdev->dev->state))
-	/*TODO __netif_schedule(dev); */ ;
+	if (test_and_clear_bit(__LINK_STATE_XOFF, &rtdev->dev->state)) {
+		struct rtnet_msg msg = {
+			.msg_type = WAKEUP,
+			.rtdev    = rtdev
+		};
+		rt_mbx_send_if(&(rt_wakeup_manager.mbx), &msg, sizeof (struct rtnet_msg));
+	}
 }
 
 static inline void rtnetif_stop_queue(struct rtnet_device *rtdev)
